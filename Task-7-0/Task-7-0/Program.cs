38cbaa113c032f314b599bec8076921aca3ea89c
Сергей <gan7er@yandex.ru>
2021-05-08 17:40:58 +0300

    Конечный вариант с тестом

--- Task-7-0/Task-7-0/Program.cs
+++ Task-7-0/Task-7-0/Program.cs
@@ -2,68 +2,74 @@
 
 namespace Task_7_0
 {
+    public class TestCase
+    {
+       public int a { get; set; }
+       public int b { get; set; }
+       public int count { get; set; }
+      
+    }
     class Program
     {
-        static int TotalP(int from,int to)
+        static void TestCount(TestCase testCase)
         {
-            if (from == to)
+            int[,] mas = new int[testCase.a, testCase.b];
+            if (testCase.count == Chess1(testCase.a, testCase.b) && testCase.count == Chess(mas.GetLength(0)-1, mas.GetLength(1)-1))
             {
-                return 1;
+                Console.WriteLine("VALID TEST");
             }
-            if (from > to)
+            else 
             {
-                return 0;
+                Console.WriteLine("INVALID TEST");
             }
-            return TotalP(from + 1, to) + TotalP(from * 2, to);
+           
+
         }
-        static int Chess(int a,int b)
+        static int Chess(int index_a,int index_b)
         {
-            if (a == 0&&b>0)
+            if (index_a == 0&&index_b>0)
             {
                 return 1;
             }
-           else if (a > 0 && b == 0)
+           else if (index_a > 0 && index_b == 0)
             {
                 return 1;
             }
-            else if (a == 0&&b==0)
+            else if (index_a == 0&&index_b==0)
             {
                 return 0;
             }
             
-            return Chess(a, b-1) + Chess(a-1, b);
+            return Chess(index_a, index_b-1) + Chess(index_a-1, index_b);
         }
-        static void Print2(int n, int m, int[,] a)
+        static int Chess1(int n,int m)
         {
-            int i, j;
-            for (i = 0; i < n; i++)
-            {
-                for (j = 0; j < m; j++)
-                    Console.Write(a[i, j]);
-                Console.Write("\r\n");
-            }
-        }
-        static void Main(string[] args)
-        {
-            int n = 5;
-            int m = 5;
-            Console.WriteLine(Chess(25, 91));
-            int[,] a = new int[5, 5];
-            
+            int[,] a = new int[n, m];
             int i, j;
             for (j = 0; j < m; j++)
-                a[0, j] = 1; // Первая строка заполнена единицами
+                a[0, j] = 1; 
             for (i = 1; i < n; i++)
             {
                 a[i, 0] = 1;
                 for (j = 1; j < m; j++)
                     a[i, j] = a[i, j - 1] + a[i - 1, j];
             }
+            return a[i - 1, j - 1];
 
+        }
+        
+        static void Main(string[] args)
+        {
+            
+            TestCase testCase = new TestCase { a = 6, b = 6,count=252 };
+            TestCase testCase1 = new TestCase { a = 1, b = 2, count = 1 };
+            TestCase testCase2 = new TestCase { a = 2, b = 1, count = 1 };
+            
+            TestCount(testCase);
+            TestCount(testCase1);
+            TestCount(testCase2);
+            
 
-            Print2(n, m,a);
-            //var total = TotalP(1, 2);
-            //Console.WriteLine(total);
         }
     }
 }
